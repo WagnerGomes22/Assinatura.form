a74e143b311c06f944b381e09b6e0f7773246ecd
Wagner-tech3 <wagnergomesdossantosw5@gmail.com>
2023-12-15 12:45:50 -0300

    att

--- Fomulário/js/script.js
+++ Fomulário/js/script.js
@@ -1,10 +1,25 @@
-const nome = document.querySelector('#nome').value;
-const sobrenome = document.querySelector('#sobrenome').value;
-const telefone = document.querySelector('#telefone').value;
-const setor = document.querySelector('#setor').value;
+function submitForm() {
 
-const resultado = `${nome} ${sobrenome}
-Telefone: ${telefone}
-Setor: ${setor}`;
+nome = document.getElementById('name').value;
+let sobrenome = document.getElementById('sobrenome').value;
+let setor = document.getElementById('setor').value;
+let imgElement = document.createElement('img');
+imgElement.src = 'https://ci3.googleusercontent.com/meips/ADKq_NanMzCf-dM-fn_ClZVMsDVH7sGt40mH28i-A_dHOPnvlzdtJfJQ9Egh00l_OE_6ThF0I-3_DMgwYmOGnhujptoY7Snw9J5R1Ew1ZjrRSAIAvqWu3_Rgpx3UtwL-OtNLVFciAqLNJjCr_O5mZlq93CFO1FwJax2Rp_W3fNgN9rLKOlFuEo5iC2kWzC0obo2891jKo5dRSNYZoXecCjMCj5k=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1gx2nvK0OA60wvfzFMAryOKdp8VSx8RXV&amp;revid=0B_KI6abtufKubWROQjAzSmxlMjFGc3Z1M1JzL2greHZPa1JnPQ';
+imgElement.width = 150;
 
-console.log(resultado);
+if(nome === '' || sobrenome === '' || setor === '') {
+    alert('Por favor, preencha todos os campos.');
+    return;
+}
+
+let assinatura = '<b style="color:rgb(255,153,0);font-size:12.8px">______________________________<wbr>___________</b>' + '<br><br>' + `<strong> ${nome} </strong>` + '&nbsp'+ `<strong> ${sobrenome} </strong>` + '<br><br>' + setor + '<br><br>' + 'Tel. + 55 11 2364.9621' + '<br><br>' + '<a href="https://www.google.com.br/maps/place/Av.+Dr.+Cardoso+de+Melo,+878+-+Vila+Olimpia,+S%C3%A3o+Paulo+-+SP/@-23.5994907,-46.6845672,17z/data=!3m1!4b1!4m5!3m4!1s0x94ce574db507d2a5:0x1532f65db5b9bcc7!8m2!3d-23.5994907!4d-46.6823785?shorturl=1">Av. Cardoso de Melo 878 - Vila Olímpia - São Paulo - SP 04548-003</a>' + '<br><br>' 
++ 'Visite nossos canais:  <a href="https://www.google.com/url?q=https%3A%2F%2Fapis3.com.br%2F&sa=D&sntz=1&usg=AFQjCNGrdVRAS4aXMeBle3ZGgWzsoRowIg">Site</a>  /  <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.facebook.com%2Fapis3oficial%2F&sa=D&sntz=1&usg=AFQjCNEfGJb1kooKxeQDvBknllnUGA-ePA">Facebook</a>  /  <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.instagram.com%2Fapis3oficial%2F&sa=D&sntz=1&usg=AFQjCNFo_9x1n7MClVvBR1Q3DFqEU_PRaA">Instagram</a>  /  <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fapis3%2F&sa=D&sntz=1&usg=AFQjCNFDYUqjdvH4u_v8rGBDibVzDdCiew">LinkedIn</a>  /  <a href="https://www.youtube.com/apis3">YouTube</a>' + 
+'<br><br>';
+document.getElementById('resultado').innerHTML = assinatura;
+document.getElementById('resultado').appendChild(imgElement);
+
+document.getElementById('nome').value = '';
+document.getElementById('sobrenome').value = '';
+document.getElementById('setor').value = '';
+
+}
